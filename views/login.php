097065192ac57ba5a6b71a9ca750fb3086283f1c
Tu Nombre <simongpj7@gmail.com>
2025-05-25 17:13:55 -0500

    Obligatorio llenar la informacion

--- views/login.php
+++ views/login.php
@@ -4,8 +4,8 @@
 <body>
     <h2>Iniciar Sesión</h2>
     <form action="../controllers/AuthController.php" method="POST">
-        Usuario: <input type="text" name="usuario"><br>
-        Contraseña: <input type="password" name="password"><br>
+        Usuario: <input type="text" name="usuario" required><br>
+        Contraseña: <input type="password" name="password" required><br>
         <button type="submit">Entrar</button>
     </form>
     <?php if (isset($_GET['error'])) echo "<p style='color:red;'>Credenciales incorrectas.</p>"; ?>
